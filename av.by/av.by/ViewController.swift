93bfefe863a67f6a2a3c479440f06a532ca48ea0
romadvestitri <79081931+romadvestitri@users.noreply.github.com>
2023-12-04 16:13:23 +0300

    init

---
+++ av.by/av.by/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  av.by
+//
+//  Created by Роман on 4.12.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
